b8e16ae61d27b74f0d951a4d9e0f15a6378b03af
thedocbwarren <jedi.master@augmentedjs.com>
2019-09-11 14:57:18 -0700

    sh file

--- dist/index.js
+++ dist/index.js
@@ -86,6 +86,62 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/colors.js":
+/*!***********************!*\
+  !*** ./src/colors.js ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+const Reset = "\x1b[0m";
+const Bright = "\x1b[1m";
+const Dim = "\x1b[2m";
+const Underscore = "\x1b[4m";
+const Blink = "\x1b[5m";
+const Reverse = "\x1b[7m";
+const Hidden = "\x1b[8m";
+const FgBlack = "\u001b[38;5;0m";
+const FgRed = "\u001b[38;5;31m";
+const FgGreen = "\u001b[38;5;2m";
+const FgYellow = "\u001b[38;5;3m";
+const FgBlue = "\u001b[38;5;4m";
+const FgMagenta = "\u001b[38;5;5m";
+const FgCyan = "\u001b[38;5;6m";
+const FgGray = "\u001b[38;5;7m";
+const FgDarkGray = "\u001b[38;5;8m";
+const FgBrightRed = "\u001b[38;5;9m";
+const FgBrightGreen = "\u001b[38;5;10m";
+const FgBrightYellow = "\u001b[38;5;11m";
+const FgBrightBlue = "\u001b[38;5;12m";
+const FgBrightMagenta = "\u001b[38;5;13m";
+const FgBrightCyan = "\u001b[38;5;14m";
+const FgWhite = "\x1b[97m";
+const BgBlack = "\u001b[48;5;0m";
+const BgRed = "\u001b[48;5;31m";
+const BgGreen = "\u001b[48;5;2m";
+const BgYellow = "\u001b[48;5;3m";
+const BgBlue = "\u001b[48;5;4m";
+const BgMagenta = "\u001b[48;5;5m";
+const BgCyan = "\u001b[48;5;6m";
+const BgGray = "\u001b[48;5;7m";
+const BgDarkGray = "\u001b[48;5;8m";
+const BgBrightRed = "\u001b[48;5;9m";
+const BgBrightGreen = "\u001b[48;5;10m";
+const BgBrightYellow = "\u001b[48;5;11m";
+const BgBrightBlue = "\u001b[48;5;12m";
+const BgBrightMagenta = "\u001b[48;5;13m";
+const BgBrightCyan = "\u001b[48;5;14m";
+const BgWhite = "\x1b[107m";
+const PALETTE = [`${BgBlack}`, `${BgBrightRed}`, `${BgBrightGreen}`, `${BgBrightBlue}`, `${BgBrightYellow}`, `${BgBrightMagenta}`, `${BgBrightCyan}`, `${BgWhite}`, `${BgDarkGray}`, `${BgRed}`, `${BgGreen}`, `${BgBlue}`, `${BgYellow}`, `${BgMagenta}`, `${BgCyan}`, `${BgGray}`];
+const PALETTE_FG = [`${FgBlack}`, `${FgBrightRed}`, `${FgBrightGreen}`, `${FgBrightBlue}`, `${FgBrightYellow}`, `${FgBrightMagenta}`, `${FgBrightCyan}`, `${FgWhite}`, `${FgDarkGray}`, `${FgRed}`, `${FgGreen}`, `${FgBlue}`, `${FgYellow}`, `${FgMagenta}`, `${FgCyan}`, `${FgGray}`];
+module.exports = {
+  "PALETTE": PALETTE,
+  "PALETTE_FG": PALETTE_FG,
+  "Reset": Reset
+};
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
@@ -128,41 +184,14 @@ if (process.argv && process.argv.length >= 3) {
   !*** ./src/printBitmap.js ***!
   \****************************/
 /*! no static exports found */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-const Reset = "\x1b[0m";
-const Bright = "\x1b[1m";
-const Dim = "\x1b[2m";
-const Underscore = "\x1b[4m";
-const Blink = "\x1b[5m";
-const Reverse = "\x1b[7m";
-const Hidden = "\x1b[8m";
-const FgBlack = "\u001b[38;5;0m";
-const FgRed = "\u001b[38;5;1m";
-const FgGreen = "\u001b[38;5;2m";
-const FgYellow = "\u001b[38;5;3m";
-const FgBlue = "\u001b[38;5;4m";
-const FgMagenta = "\u001b[38;5;5m";
-const FgCyan = "\u001b[38;5;6m";
-const FgWhite = "\u001b[38;5;7m";
-const BgBlack = "\u001b[48;5;0m";
-const BgRed = "\u001b[48;5;1m";
-const BgGreen = "\u001b[48;5;2m";
-const BgYellow = "\u001b[48;5;3m";
-const BgBlue = "\u001b[48;5;4m";
-const BgMagenta = "\u001b[48;5;5m";
-const BgCyan = "\u001b[48;5;6m";
-const BgGray = "\u001b[48;5;7m";
-const BgDarkGray = "\u001b[48;5;8m";
-const BgBrightRed = "\u001b[48;5;9m";
-const BgBrightGreen = "\u001b[48;5;10m";
-const BgBrightYellow = "\u001b[48;5;11m";
-const BgBrightBlue = "\u001b[48;5;12m";
-const BgBrightMagenta = "\u001b[48;5;13m";
-const BgBrightCyan = "\u001b[48;5;14m";
-const BgWhite = "\x1b[47m";
-const PALETTE = [`${BgBlack}`, `${BgBrightRed}`, `${BgBrightGreen}`, `${BgBrightBlue}`, `${BgBrightYellow}`, `${BgBrightMagenta}`, `${BgBrightCyan}`, `${BgWhite}`, `${BgDarkGray}`, `${BgRed}`, `${BgGreen}`, `${BgBlue}`, `${BgYellow}`, `${BgMagenta}`, `${BgCyan}`, `${BgGray}`];
-const PALETTE_FG = [`${FgBlack}`, `${FgBrightRed}`, `${FgBrightGreen}`, `${FgBrightBlue}`, `${FgBrightYellow}`, `${FgBrightMagenta}`, `${FgBrightCyan}`, `${FgWhite}`, `${FgDarkGray}`, `${FgRed}`, `${FgGreen}`, `${FgBlue}`, `${FgYellow}`, `${FgMagenta}`, `${FgCyan}`, `${FgGray}`];
+const {
+  PALETTE,
+  PALETTE_FG,
+  Reset
+} = __webpack_require__(/*! ./colors.js */ "./src/colors.js");
+
 const SQUARE = " ";
 const HALF = "▀";
 
@@ -183,8 +212,11 @@ const printDouble = (row1, row2) => {
   let x = 0,
       text = "";
 
-  for (x = 0; x < row.length; x++) {
-    text += `${PALETTE[row2[x]]}${PALETTE_FG[row1[x]]}${HALF}`;
+  for (x = 0; x < row1.length; x++) {
+    if (row1 && row2) {
+      text += `${PALETTE[row2[x]]}\x1b[2m${PALETTE_FG[row1[x]]}${HALF}`;
+    } else {// nothing
+    }
   }
 
   text += Reset;
---
+++ src/colors.js
@@ -0,0 +1,88 @@
+const Reset = "\x1b[0m";
+const Bright = "\x1b[1m";
+const Dim = "\x1b[2m";
+const Underscore = "\x1b[4m";
+const Blink = "\x1b[5m";
+const Reverse = "\x1b[7m";
+const Hidden = "\x1b[8m";
+
+const FgBlack = "\u001b[38;5;0m";
+const FgRed = "\u001b[38;5;1m";
+const FgGreen = "\u001b[38;5;2m";
+const FgYellow = "\u001b[38;5;3m";
+const FgBlue = "\u001b[38;5;4m";
+const FgMagenta = "\u001b[38;5;5m";
+const FgCyan = "\u001b[38;5;6m";
+const FgGray = "\u001b[38;5;7m";
+
+const FgDarkGray = "\u001b[38;5;8m";
+const FgBrightRed = "\u001b[38;5;9m";
+const FgBrightGreen = "\u001b[38;5;10m";
+const FgBrightYellow = "\u001b[38;5;11m";
+const FgBrightBlue = "\u001b[38;5;12m";
+const FgBrightMagenta = "\u001b[38;5;13m";
+const FgBrightCyan = "\u001b[38;5;14m";
+const FgWhite = "\x1b[97m";
+
+const BgBlack = "\u001b[48;5;0m";
+const BgRed = "\u001b[48;5;1m";
+const BgGreen = "\u001b[48;5;2m";
+const BgYellow = "\u001b[48;5;3m";
+const BgBlue = "\u001b[48;5;4m";
+const BgMagenta = "\u001b[48;5;5m";
+const BgCyan = "\u001b[48;5;6m";
+const BgGray = "\u001b[48;5;7m";
+
+const BgDarkGray = "\u001b[48;5;8m";
+const BgBrightRed = "\u001b[48;5;9m";
+const BgBrightGreen = "\u001b[48;5;10m";
+const BgBrightYellow = "\u001b[48;5;11m";
+const BgBrightBlue = "\u001b[48;5;12m";
+const BgBrightMagenta = "\u001b[48;5;13m";
+const BgBrightCyan = "\u001b[48;5;14m";
+const BgWhite = "\x1b[107m";
+
+const PALETTE = [
+  `${BgBlack}`,
+  `${BgBrightRed}`,
+  `${BgBrightGreen}`,
+  `${BgBrightBlue}`,
+  `${BgBrightYellow}`,
+  `${BgBrightMagenta}`,
+  `${BgBrightCyan}`,
+  `${BgWhite}`,
+  `${BgDarkGray}`,
+  `${BgRed}`,
+  `${BgGreen}`,
+  `${BgBlue}`,
+  `${BgYellow}`,
+  `${BgMagenta}`,
+  `${BgCyan}`,
+  `${BgGray}`
+];
+
+const PALETTE_FG = [
+  `${FgBlack}`,
+  `${FgBrightRed}`,
+  `${FgBrightGreen}`,
+  `${FgBrightBlue}`,
+  `${FgBrightYellow}`,
+  `${FgBrightMagenta}`,
+  `${FgBrightCyan}`,
+  `${FgWhite}`,
+  `${FgDarkGray}`,
+  `${FgRed}`,
+  `${FgGreen}`,
+  `${FgBlue}`,
+  `${FgYellow}`,
+  `${FgMagenta}`,
+  `${FgCyan}`,
+  `${FgGray}`
+];
+
+module.exports = {
+  "PALETTE": PALETTE,
+  "PALETTE_FG": PALETTE_FG,
+  "RESET": Reset,
+  "DIM": Dim,
+};
