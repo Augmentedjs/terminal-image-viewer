f775c00958a71fd003d7f8d377f2797380055bcd
thedocbwarren <jedi.master@augmentedjs.com>
2020-01-04 18:05:46 -0800

    updates

--- test/smallConvertSpec.js
+++ test/smallConvertSpec.js
@@ -1,20 +1,21 @@
-const {printBitmapSingle, printBitmapDouble} = require("../src/printBitmap.js");
+const printSingle = require("../src/printSingle.js");
+const printDouble = require("../src/printDouble.js");
 
 
 describe("Given bitmaps in json", () => {
   it("can print to console", async () => {
     const json = require("../data/apples.json");
-    const image = await printBitmapDouble(json);
+    const image = await printSingle(json);
     expect(image).to.not.be.undefined;
   });
   it("can print to console", async () => {
     const json = require("../data/peppers.json");
-    const image = await printBitmapDouble(json);
+    const image = await printDouble(json);
     expect(image).to.not.be.undefined;
   });
   it("can print to console", async () => {
     const json = require("../data/wheel.json");
-    const image = await printBitmapSingle(json);
+    const image = await printSingle(json);
     expect(image).to.not.be.undefined;
   });
 });
